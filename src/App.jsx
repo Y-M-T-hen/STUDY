9403d687a9509c68118382958c59429a45f51418
Yusuf Mert <yercetin271@gmail.com>
2025-06-08 16:35:31 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function Uyg1() {
+ const players = [{
+  ad:"yusuf",
+  soyad:"ercetin"
+ },2,3,4,5]
+
+  return (
+    <div id='general'>
+     <p id='p'>oyuncu = {players[0[2]]}</p>
+    </div>
+
+  );
+}
+
+export default Uyg1
